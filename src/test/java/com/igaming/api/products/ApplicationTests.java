861ab2ddfc0f32a95c5c33f2065abb57057d8a3b
Sergey Chupin <schupin@alfabank.ru>
2020-09-15 18:41:07 +0300

    chore: init project

---
+++ src/test/java/com/igaming/api/products/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igaming.api.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
